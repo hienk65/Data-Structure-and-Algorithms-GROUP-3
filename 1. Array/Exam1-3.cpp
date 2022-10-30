f5134cdab44aff579bb1aa4ecd01ee925aa87a92
hienk65 <92668114+hienk65@users.noreply.github.com>
2022-10-30 17:01:58 +0700

    Update Exam1-3.cpp

--- 1. Array/Exam1-3.cpp
+++ 1. Array/Exam1-3.cpp
@@ -1,3 +1,78 @@
+Code:
+#include <iostream>
+#include <ctime>
+
+#define N 15
+
+using namespace std;
+
+void merge(int *,int, int , int );
+
+void merge_sort(int *arr, int low, int high)
+{
+    if (low < high) {
+
+        // divide array at mid and independently sort them using merge sort
+        int mid = (low + high) / 2;
+        merge_sort(arr, low, mid);
+        merge_sort(arr, mid + 1, high);
+
+        // merge / conquer sorted arrays
+        merge(arr, low, high, mid);
+    }
+}
+
+// Merge sort 
+void merge(int *arr, int low, int high, int mid)
+{
+    int k = low, i = low, j = mid + 1, aux[N];
+
+    while (i <= mid && j <= high) {
+        if (arr[i] < arr[j]) {
+            aux[k++] = arr[i++];
+        }
+        else  {
+            aux[k++] = arr[j++];
+        }
+    }
+
+    // copy remaining elements
+    while (i <= mid) {
+        aux[k++] = arr[i++];
+    }
+    while (j <= high) {
+        aux[k++] = arr[j++];
+    }
+    
+    for (int i = low; i <= high; i++)  {
+        arr[i] = aux[i];
+    }
+}
+
+int main()
+{   
+    int arr[N];
+    srand(time(NULL));
+ 
+    // generate random input of integers
+    for (int i = 0; i < N; i++) {
+        arr[i] = (rand() % 100) - 50;
+    }
+ 
+    merge_sort(arr, 0, N - 1);
+    
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+
+
+
+
+
+Algorithm:
+
 Function printAllSubarrays (A, n)
 For i=0 to n do {
   Sum =0
